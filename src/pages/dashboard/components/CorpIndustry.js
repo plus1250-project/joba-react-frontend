b6df1484c8334e29d5daab7b76147519e2f3dee6
Jonathan <jonnygim.dev@gmail.com>
2022-11-15 20:46:25 +0900

    Refactor: 레이아웃 수정
    창 크기 변경시 레이아웃 변경이 틀어지기 때문에 수정함

--- src/pages/dashboard/components/CorpIndustry.js
+++ src/pages/dashboard/components/CorpIndustry.js
@@ -12,10 +12,11 @@ const CorpIndustry = (props) => {
   return (
     <Widget className="widget-p-sm"  >
         <div className={`${s.smallWidget} ${s.pointer}`} onClick={props.onOpen}>
-            <div className="d-flex mb-4">
+            <div className="d-flex mb-3">
               <img className="py-1 ml-2 mr-2 mt-4 img-fluid" src={heartYellow} alt="..." style={{width :"15%"}}/>
               <div className="d-flex flex-column">
-                  <p className="headline-1 ml-4" style={{margin :"33px 0 0 0"}}>성장 기업 리스트</p>                
+                  <p className="headline-2 mt-2 ml-4">{props.industryName}</p>             
+                  <p className="headline-1 ml-4 mt-2">성장 기업</p>   
               </div>
             </div>
         </div>
